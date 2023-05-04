b727a45142b3b6345ef923a3814caf5d8b663bfa
HiddenInTheClouds <joshzabihian@gmail.com>
2023-05-03 20:39:26 -0500

    added notes for each variable, function, and array. changed const to variables for characters being used in password, added password length if statement, added password length prompt, and added function for getting random array.

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,102 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Password Values
-const lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-const uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-const numbers = ["1234567890"];
-const specialchar = ["!@#$%^&*()_+"];
-
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Array of special characters to be included in password
+var specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+];
+// Function for getting a random element from an array
+function getRandom(arr) {}
+// Function to generate password with user input
 function generatePassword() {
-  console.log("Hey! You clicked the button!");
+  var lengthPassword = parseInt(prompt("What's the length of the password?"));
+  console.log(lengthPassword);
 
-  // 1. Prompt the user for the password criteria
-  //    a. Password Length 8 < 128
-  //    b. Lowercase, uppercase, numbers, special characters
-  let lowercase = confirm("Do you want to use lowercase letters?");
-  let uppercase = confirm("Do you want to use uppercase letters?");
-  let numbers = confirm("Do you want to use numbers?");
-  let specialchar = confirm("Do you want to use special characters?");
-  let passwordLength = confirm;
-  if (x <= 8 || x >= 128) {
-    Return;
+  var lowerChar = confirm("Do you want to use lowercase letters?");
+  console.log(lowerChar);
+  var upperChar = confirm("Do you want to use uppercase letters?");
+  console.log(upperChar);
+  var numChar = confirm("Do you want to use numbers?");
+  console.log(numChar);
+  var specialChar = confirm("Do you want to use special characters?");
+  console.log(specialChar);
+  if (lengthPassword < 8) {
+    alert("Choose number greater than 8");
+  }
+  if (lengthPassword > 128) {
+    alert("Choose number less than 128");
   }
 
-  // 2. Validate the input.
-  // 3. Generate password based on criteria.
-  // 4. Display password to the page.
   return "Generated password will go here";
 }
 // Write password to the #password input
@@ -36,3 +109,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// 1. Prompt the user for the password criteria
+//    a. Password Length 8 < 128
+//    b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input.
+// 3. Generate password based on criteria.
+// 4. Display password to the page.
